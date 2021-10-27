c67de53f97b04ab9cf2e6b281d80bb768165a88c
MirMara <92723885+MirMara@users.noreply.github.com>
2021-10-27 22:11:01 +0200

    Add files via upload
    not working

---
+++ 27.10.21/JVS/script.js
@@ -0,0 +1,12 @@
+function myFunction(){
+
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+
+    
+}
+
+function illuminate(){
+    const element = document.getElementById(cont3);
+    element.classList.add("bright");
+}
